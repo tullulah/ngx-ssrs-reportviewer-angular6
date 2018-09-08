00db7a679e1bc2652030be28fa6cb586de23cb4c
Dani <tullulah@gmail.com>
2018-09-08 18:24:58 +0200

    updated to use token

--- src/app/modules/reportviewer/reportviewer.component.ts
+++ src/app/modules/reportviewer/reportviewer.component.ts
@@ -1,7 +1,8 @@
 import {
-  Component, Input, Output, ElementRef, EventEmitter, OnChanges, SimpleChanges
+  Component, Input, Output, ElementRef, EventEmitter, OnChanges, SimpleChanges, ViewChild
 } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { HttpClient , HttpHeaders, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'ssrs-reportviewer',
@@ -9,77 +10,92 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./reportviewer.component.css']
 })
 export class ReportViewerComponent implements OnChanges {
-
+  @ViewChild('iframe') iframe: ElementRef;
 
   @Input()
   reporturl: string;
   @Input()
   reportserver: string;
   @Input()
-  showparameters?: string = "false";
+  showparameters = 'false';
   @Input()
   parameters?: any;
   @Input()
-  language?: string = "en-us";
+  language = 'en-us';
   @Input()
-  width?: number = 100;
+  width = 100;
   @Input()
-  height?: number = 100;
+  height = 100;
   @Input()
-  toolbar?: string = "true";
+  toolbar = 'true';
 
 
   @Output('error') onError = new EventEmitter<any>();
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(private sanitizer: DomSanitizer, private http: HttpClient) {
   }
 
 
   source: SafeResourceUrl;
 
   ngOnChanges(changes: SimpleChanges) {
-    if(!this.reporturl){
-      this.onError.emit("Src cannot be null");
+    if (!this.reporturl) {
+      this.onError.emit('Src cannot be null');
     }
 
     if ('reporturl' in changes) {
-      this.source = this.sanitizer
-      .bypassSecurityTrustResourceUrl(this.buildReportUrl());
+      //this.source = this.sanitizer.bypassSecurityTrustResourceUrl(this.buildReportUrl());
+
+      this.get(this.buildReportUrl(), null).subscribe(data => this.iframe.nativeElement.src = data);
     }
   }
 
+  createAuthorizationHeader() {
+    const token = sessionStorage.getItem('access_token');
+    return new HttpHeaders()
+      .set('Authorization', 'Bearer ' + token)
+      .set('X-Origin', 'SomDenWeb').set('Allow-Control-Allow-Origin', '*');
+  }
+
+  get(url: string, params: HttpParams) {
+    return this.http.get(url.replace('{hostname}', window.location.hostname), {params: params, headers: this.createAuthorizationHeader() })
+    // .catch(err => /*this.handleError(err)*/ )
+    ;
+  }
+
+
   public buildParameterString(): string {
 
-    var parameterString = "";
+    let parameterString = '';
 
-    for (var param in this.parameters) {
+    for (const param in this.parameters) {
       if (param) {
         if (this.parameters[param] instanceof Array === true) {
-          for (var arrayParam in this.parameters[param]) {
+          for (const arrayParam in this.parameters[param]) {
             if (arrayParam) {
-              parameterString += "&" + param + "=" + this.parameters[param][arrayParam];
+              parameterString += '&' + param + '=' + this.parameters[param][arrayParam];
             }
           }
         }
         if (this.parameters[param] != null && this.parameters[param] instanceof Array === false) {
-          parameterString += "&" + param + "=" + this.parameters[param];
+          parameterString += '&' + param + '=' + this.parameters[param];
         }
         if (this.parameters[param] == null) {
-          parameterString += "&" + param + ":isnull=true";
+          parameterString += '&' + param + ':isnull=true';
         }
       }
     }
     return parameterString;
   }
 
-  public buildReportUrl() : string {
+  public buildReportUrl(): string {
     if (!this.reporturl) {
       return;
     }
-    var parameters = this.buildParameterString();
+    const parameters = this.buildParameterString();
     return this.reportserver + '?/'
       + this.reporturl + '&rs:Embed=true'
       + '&rc:Parameters=' + this.showparameters
       + parameters
-      + '&rs:ParameterLanguage=' + this.language + "&rc:Toolbar=" + this.toolbar;
+      + '&rs:ParameterLanguage=' + this.language + '&rc:Toolbar=' + this.toolbar;
   }
 }
